d166811ab4048ae9635ae93a4ff8c4a11817215a
Yash Ghugardare <117991996+yghugardare@users.noreply.github.com>
2023-10-14 11:44:23 +0000

    oops1

---
+++ OOPS1.java
@@ -0,0 +1,10 @@
+c
+public class OOPS1{
+    
+    public static void main(String[] args) {
+        Book b1 = new Book("harry potter", "JK ROWLING", 1998);
+        Book b2 = new Book("Gita", "Krisha", 2002);
+        b1.displayInfo();
+        b2.displayInfo();
+    }
+}
